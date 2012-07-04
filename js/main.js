25402a1c5ffed599b4b09d06325fc5b81bcf4928
katspaugh <ivan.kalyaev@gmail.com>
2012-07-04 18:44:36 +0400

    rm impress.js

--- js/main.js
+++ js/main.js
@@ -10,13 +10,70 @@ String.prototype.template = function (o) {
 (function () {
 	var box = document.querySelector('#impress');
 
+	var init = function () {
+		var steps = box.querySelectorAll('.step');
+		var menus = box.querySelectorAll('.menu');
+		var menu1 = menus[0];
+		var menu2 = menus[1];
+
+		var currentStep;
+		var currentItem;
+		var defClick;
+		var shaded = true;
+
+		menu1.style.left = ~~(menu1.offsetParent.clientWidth / 2) + 'px';
+		menu1.classList.remove('zoomed');
+
+		menu1.addEventListener('click', function (e) {
+			if (shaded) {
+				menu1.classList.add('shaded');
+				menu2.classList.remove('rotated');
+
+				defClick();
+			} else {
+				menu1.classList.remove('shaded');
+				menu2.classList.add('rotated');
+
+				currentStep.classList.add('rotated');
+				currentItem.classList.remove('active');
+				currentStep = null;
+				currentItem = null;
+			}
+
+			shaded = !shaded;
+		}, false);
+
+		[].forEach.call(
+			menu2.querySelectorAll('.item'),
+			function (el, i, items) {
+				var onClick = function (e) {
+					if (currentStep) {
+						currentStep.classList.add('rotated');
+					}
+					currentStep = steps[i];
+					currentStep.classList.remove('rotated');
+
+					if (currentItem) {
+						currentItem.classList.remove('active');
+					}
+					currentItem = items[i];
+					currentItem.classList.add('active');
+				};
+
+				el.addEventListener('click', onClick, false);
+
+				if (0 == i) {
+					defClick = onClick;
+				}
+			}
+		);
+	};
+
 	var applyTemplate = function (obj) {
 		box.innerHTML = box.innerHTML.template(obj);
 
 		var sides = box.querySelectorAll('div');
 		var prevCls;
-
-		impress().init();
 	};
 
 	var xhr = new XMLHttpRequest();
@@ -25,6 +82,7 @@ String.prototype.template = function (o) {
 		var resp = xhr.responseText;
 		var json = JSON.parse(resp);
 		applyTemplate(json);
+		init();
 	});
 	xhr.send();
 }());
